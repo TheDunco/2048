c54b50e2d84ad51b5b2f14d1c7e4249c75dda2d4
TheDunco <duncvan@gmail.com>
2018-11-30 11:47:59 -0500

    Started writing main game file

--- 2048.py
+++ 2048.py
@@ -3,3 +3,26 @@ CS108 Final Project
 Created Fall 2018
 @author: Duncan Van Keulen
 '''
+
+from GUI import *
+from tile import *
+from tkinter import *
+
+
+class Twenty48:
+
+    def __init__(self):
+        self._occupied_tiles = [False, False, False, False,
+                                False, False, False, False,
+                                False, False, False, False,
+                                False, False, False, False]
+        root = Tk()
+        root.title('2048')
+        app = GUI(root)
+        root.mainloop()
+
+
+if __name__ == '__main__':
+    game = Twenty48()
+    
+
